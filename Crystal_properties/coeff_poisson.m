bbb245fe0c4933b2aa82bad5760544e7b4842ab4
unknown <d.mercier@mapc103.mpie.de>
2015-04-15 11:37:34 +0200

    added Matlab functions related to crystal properties

---
+++ Crystal_properties/coeff_poisson.m
@@ -0,0 +1,22 @@
+function nu = coeff_poisson(C12, C44, varargin)
+% C12 and C14 : compliances in TPa
+% nu: Poisson's coefficient
+
+% author: d.mercier@mpie.de
+
+% [LEVY 2001] : "Handbook of elastic properties of solids, liquids and
+% gases" by M. Levy, H.E. Bass and R.S. Stern (Vol. 2 - Elastic
+% Properties of Solids: Theory, Elements, and Compounds, Novel Materials,
+% Technological Materials, Alloys, Building Materials)
+% Chapter 1 / p. 19
+% ISBN-13: 978-0124457607  ISBN-10: 0124457606
+
+if nargin < 2
+    % Elastic constants of the Titanium (hcp)
+    C12 = 0.0903;
+    C44 = 0.0465;
+end
+
+nu = 1/(2*(1+(C44/C12)));
+
+end
---
+++ Crystal_properties/shear_modulus.m
@@ -0,0 +1,21 @@
+% Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
+function G = shear_modulus(C12, C44, varargin)
+% E : Young's modulus in TPa
+% nu: Poisson's coefficient
+% G : Shear modulus in TPa
+
+% author: d.mercier@mpie.de
+
+if nargin < 2
+    % Elastic constants of the Titanium (hcp)
+    C12 = 0.0903;
+    C44 = 0.0465;
+end
+
+E = C44*(3*C12 + 2*C44)/(C12 + C44);
+
+nu = coeff_poisson(C12, C44);
+
+G = E/(2*(1+nu));
+
+end
