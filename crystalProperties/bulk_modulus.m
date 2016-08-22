ac284175770d66e9058275ad043cac934b202fb6
David Mercier <david9684@gmail.com>
2016-08-22 19:05:26 +0200

    added function for bulk modulus

---
+++ crystalProperties/bulk_modulus.m
@@ -0,0 +1,21 @@
+% Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
+function K = bulk_modulus(C12, C44, varargin)
+% E : Young's modulus in TPa
+% nu: Poisson's coefficient
+% K : Shear modulus in TPa
+
+% author: david.mercier@crmgroup.be
+
+if nargin < 2
+    % Elastic constants of the Titanium (hcp)
+    C12 = 0.0903;
+    C44 = 0.0465;
+end
+
+E = C44*(3*C12 + 2*C44)/(C12 + C44);
+
+nu = coeff_poisson(C12, C44);
+
+K = E/(3*(1-(2*nu)));
+
+end
