6d29b7fe425b6f4b37988209261a996012963e69
David Mercier <DavidMercier@users.noreply.github.com>
2019-09-24 17:26:06 +0200

    Update ROMComparison.m

--- ruleMixtures/ROMComparison.m
+++ ruleMixtures/ROMComparison.m
@@ -2,6 +2,7 @@
 %% Script to compare rules of mixtures
 % From "Mechanical Behavior of Materials", by Meyers M and Chawla K.,
 % 2nd edition, 2008.
+% From F. Akhtar, Canadian Metallurgical Quarterly 2014 VOL 53 NO 3 253
 
 % author: david9684@gmail.com
 
@@ -15,10 +16,20 @@ voigtAverage = volumeFraction(:) .* Ef + (1-volumeFraction(:)) .* Em;
 reussAverage = ((volumeFraction(:) ./ Ef) + ((1-volumeFraction(:)) ./ Em)).^(-1);
 voigtReussHillAverage = (voigtAverage+reussAverage)./2;
 
+s = 1;
+q = ((Ef./Em)-1) ./ ((Ef./Em)+2*s); 
+HT = Em .* (1+2*s.*q.*volumeFraction) ./ (1-q*volumeFraction);
+
+s = 5;
+q = ((Ef./Em)-1) ./ ((Ef./Em)+2*s); 
+HT2 = Em .* (1+2*s.*q.*volumeFraction) ./ (1-q*volumeFraction);
+
 line('XData',volumeFraction,'YData',voigtAverage','Color','red','LineWidth',3); hold on;
 line('XData',volumeFraction,'YData',reussAverage','Color','blue','LineWidth',3, 'Linestyle','-.'); hold on;
 line('XData',volumeFraction,'YData',voigtReussHillAverage','Color','black','LineWidth',3, 'Linestyle',':');
-legend('Voigt', 'Reuss', 'Voigt-Reuss-Hill','Location','northwest');
+line('XData',volumeFraction,'YData',HT','Color','Green','LineWidth',3);%, 'Linestyle',':'
+line('XData',volumeFraction,'YData',HT2','Color','Magenta','LineWidth',3, 'Linestyle',':');
+legend('Voigt', 'Reuss', 'Voigt-Reuss-Hill', 'Halpin-Tsai s=1','Halpin-Tsai s=5','Location','northwest');
 xlabel('Fraction volumique des renforcements');
 ylabel('Module d''Young (GPa)');
 grid on;
